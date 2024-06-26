473311c29acbcd722728c8d2bf867c85ed2c13c8
Jade Piramide <jadepiramidepogi@gmail.com>
2024-06-26 00:05:08 -0700

    Start on code

---
+++ src/simmer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cerr << "usage: " << argv[0] << " ... ";
+
+  return 0;
+}
---
+++ src/vm/vm.h
@@ -0,0 +1,14 @@
+#ifndef __SIMMER_VM_H
+#define __SIMMER_VM_H
+
+#include "vm/state.h"
+
+class SimmerVm {
+public:
+  SimmerVm();
+
+private:
+
+};
+
+#endif // !__SIMMER_VM_H
